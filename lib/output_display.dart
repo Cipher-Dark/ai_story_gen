7766c338564a31ecf8e447df68487d511348a55f
Sagar Bisht <iamsofficials@gmail.com>
2024-11-17 03:13:03 +0530

    editable

--- lib/output_display.dart
+++ lib/output_display.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'package:ai_story_gen/story_gen_service.dart';
 
+// ignore: must_be_immutable
 class OutputDisplay extends StatefulWidget {
   String data;
   final String selectedGenre;
@@ -21,6 +24,17 @@ class _OutputDisplayState extends State<OutputDisplay> {
   String? data1;
   bool _isLoading = false;
   bool _isGenerate = false;
+  bool _isEditing = false;
+
+  TextEditingController _textEditingController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController.text = !_isGenerate ? widget.data : data1.toString();
+  }
+
   Future<void> _refreshStroy() async {
     setState(() {
       _isLoading = true;
@@ -40,6 +54,12 @@ class _OutputDisplayState extends State<OutputDisplay> {
     }
   }
 
+  void _isToggle() {
+    setState(() {
+      _isEditing = !_isEditing;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // String selectedThem = this.selectedTheme;
@@ -47,17 +67,31 @@ class _OutputDisplayState extends State<OutputDisplay> {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black87,
+        // backgroundColor: Colors.black87,
         title: const Text("Story Generated"),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(children: [
-          Text(
-            _isGenerate ? data1.toString() : widget.data,
-            style: const TextStyle(fontSize: 16.0),
-          ),
-          ElevatedButton(onPressed: () {}, child: const Icon(Icons.add))
+          _isEditing
+              ? TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null, // Allows dynamic height adjustment
+                  minLines: 1,
+                  scrollController: _scrollController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Edit Story"),
+                )
+              : Text(
+                  _textEditingController.text,
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+          ElevatedButton(
+              onPressed: _isToggle,
+              child: _isEditing
+                  ? const Icon(Icons.next_plan)
+                  : const Icon(Icons.edit))
         ]),
       ),
       floatingActionButton: FloatingActionButton(
